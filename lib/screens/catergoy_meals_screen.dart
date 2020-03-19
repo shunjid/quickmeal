8374cb131723bae51b1a0e288d8136d1d39d0945
shunjid <shunjid1862@gmail.com>
2020-03-19 21:19:16 +0600

    mapped selected meals for each category

--- lib/screens/catergoy_meals_screen.dart
+++ lib/screens/catergoy_meals_screen.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:quickmeal/data/meals_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/categoriesMealsScreen';
 
   @override
   Widget build(BuildContext context) {
-    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
+    final routeArgs =ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
+
     final String selectedCategoryId = routeArgs['selectedCategoryId'];
     final String selectedCategoryTitle = routeArgs['selectedCategoryTitle'];
+    final mealsOfSelectedCategory = DUMMY_MEALS.where((eachMeal) {
+      return eachMeal.categories.contains(selectedCategoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(selectedCategoryTitle),
       ),
-      body: Center(
-        child: Text(
-          'The Receipes For The Category',
-        ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Text(
+            mealsOfSelectedCategory[index].title,
+          );
+        },
+        itemCount: mealsOfSelectedCategory.length,
       ),
     );
   }
