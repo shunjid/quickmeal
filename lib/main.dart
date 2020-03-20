ed3ff982f399198226ee5a70fc0ce35299178025
shunjid <shunjid1862@gmail.com>
2020-03-20 14:32:07 +0600

    assembled meal details

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.pink,
         accentColor: Colors.amber,
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
